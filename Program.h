b94db7ee0e4219ed6f377f8542c058adf2eaa301
Amna Ahmed <ammy5342@gmail.com>
2023-04-18 16:05:41 +0200

    Moveed user hashmap and global functions to program classs

--- Program.h
+++ Program.h
@@ -2,14 +2,20 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
+#include "User.h"
+
 using namespace std;
 
 class Program
 {
+	char choice;
+	unordered_map<int, User> users;
 public:
 	//menu functions
-	void mainMenu();
+	void loginMenu();
 	//helper functions
+	User getUser(int id);
 	void printCentered(string str);
 
 };
