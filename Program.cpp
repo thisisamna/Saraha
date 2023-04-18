d39297c3d9c7fe476aa4cebc152a6cb23aef2844
Amna Ahmed <ammy5342@gmail.com>
2023-04-18 16:12:31 +0200

    Added login menu (source file belongs to previous commit)

--- Program.cpp
+++ Program.cpp
@@ -1,16 +1,43 @@
 #include "Program.h"
-#include "wtypes.h"
+
 #include <iostream>
 
-void Program::mainMenu()
+void Program::loginMenu()
 {
 	printCentered("S a r a h a");
 	printCentered("Annonymously send and receive messages.");
+	cout << "Welcome!" << endl;
+	cout << "Do you have an account? (y/N)\n\n" << endl; //bug here, two newlines fix it but why?
+
+	cin >>choice;
+
+	switch (choice) {
+	case 'y':
+	case 'Y':
+		printCentered("L o g   i n");
+		//login
+		break;
+	case 'n':;
+	case 'N':
+		printCentered("S i g n   u p");
+		//signup
+		break;
+	default:
+		cout << "Invalid entry, try again!";
+	}
+}
+
+User Program::getUser(int id)
+{
+	User user;
+	auto it = users.find(id);
+	user = it->second;
+	return user;
 }
 
 void Program::printCentered(string str)
 {
-	int maxwidth = 80;
+	int maxwidth = 70;
 	stringstream ss;
 	int padding = maxwidth - str.size() / 2;
 	for (int i = 0; i < padding;  i++) 
@@ -22,7 +49,6 @@ void Program::printCentered(string str)
 	{
 		ss << " ";
 	}
-	ss << endl;
-	cout << ss.str();
+	cout << ss.str() << endl;
 }
 
--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,25 @@
 #include "User.h"
 #include "Message.h"
+#include "Program.h"
 #include <iostream>
-#include <unordered_map>
+
 
 //global objects & variables
-unordered_map<int, User> users;
 
 //function declarations
-User getUser(int id);
 
 using namespace std;
 
 int main() {
+	Program program;
+
+	program.loginMenu();
 
 	return 0;
 }
 
 //function definitions
 
-User getUser(int id)
-{
-	User user;
-	auto it = users.find(id);
-	user = it->second;
-	return user;
-}
+
 
 
