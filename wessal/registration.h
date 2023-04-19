095e77aaf9fbb2540b97b6bb01d0c87b5640f5f4
wessal <111133288+wessal11@users.noreply.github.com>
2023-04-19 09:08:28 +0200

    Add files via upload

---
+++ wessal/registration.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+class registration
+{
+	string username,password;
+public:
+	registration();
+	void sign_up();
+	~registration();
+};
+
