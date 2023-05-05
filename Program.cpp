df9bda0fb6fbdabbb34b5161912b900191be1a5f
wessal1 <wessalsalah5@gmail.com>
2023-05-05 16:21:25 +0300

    file update

--- Program.cpp
+++ Program.cpp
@@ -328,19 +328,22 @@ void Program::undolastmessage(User* liveUser) {
 
 void Program::savefile() {
 	ofstream ourfile("ourdata.txt", ios::app);
-	User u;
+	Message m;
 	if (ourfile.is_open()) {
 		for (auto data : users)
 		{
-			ourfile << data.first << ":  " << data.second.username << "\t" << data.second.password << endl;
-			for (auto data : u.sent)
-			{
-				ourfile << data.getContent();
-				for (auto data : u.inbox)
-				{
-					ourfile << data.getReceiverID();
-				}
+			ourfile << data.first << ":  " << data.second.username << "\t" << data.second.password << "\t";
+			for (auto& elem : data.second.sent) {
+				ourfile << elem.getContent();
 			}
+			
+			/*for (int i = 0; i < data.second.sent.size(); i++) {
+				ourfile << data.second.sent.front() << endl;
+				data.second.sent.push_back(data.second.sent.front());
+				data.second.sent.pop_front();
+
+			}*/
+			
 		}
 	}
 		ourfile.close();
--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main() {
 	Program program;
 	program.loop();
+
 	return 0;
 }
 
